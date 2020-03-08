f4c268131e6abd37f668e7c7368b0112d6ad2a00
Ryan Elliott <ryan.d.elliott@gmail.com>
2020-03-08 15:25:21 -0600

    add cors middleware

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,16 +1,17 @@
 import "reflect-metadata";
 import { createConnection } from "typeorm";
 import express from "express";
+import cors from "cors";
 import { advertiserSourceRouter, productRouter } from "./routes";
 
 createConnection()
   .then(async connection => {
     const app = express();
     const port = process.env.PORT || 3000;
-
+    app.use(cors());
     // define a route handler for the default home page
     app.get("/", (req, res) => {
-      res.send("Hellooo world!");
+      res.send("OK");
     });
 
     app.use(express.json());
